0bd0978db56ccabb730be16bdd489242f9bb6a4a
Thanaphum Thepwan <thanaphum.tw@gmail.com>
2023-02-07 18:30:21 +0700

    connect with MongoDB Atlas

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const connectDB = require('./config/db');
 const hospitals = require('./routes/hospitals');
 dotenv.config({path:'./config/config.env'});
 
+connectDB();
+
 const app = express();
 
+app.use(express.json());
 app.use('/api/v1/hospitals', hospitals);
 
 const PORT=process.env.PORT || 5001;
-app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, 'mode on port', PORT));
+const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, 'mode on port', PORT));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection',(err,promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(()=>process.exit(1));
+})
