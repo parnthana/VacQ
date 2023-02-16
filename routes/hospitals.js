bbc3c2d55b0e53a9328d0f04d892e348cd28e476
Thanaphum Thepwan <thanaphum.tw@gmail.com>
2023-02-16 20:32:02 +0700

    style: reformat code using prettier

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,7 +1,17 @@
-const express = require('express');
-const {getHospitals, getHospital, createHospital, updateHospital, deleteHospital} = require('../controllers/hospitals');
+const express = require("express");
+const {
+  getHospitals,
+  getHospital,
+  createHospital,
+  updateHospital,
+  deleteHospital,
+} = require("../controllers/hospitals");
 const router = express.Router();
-router.route('/').get(getHospitals).post(createHospital);
-router.route('/:id').get(getHospital).put(updateHospital).delete(deleteHospital);
+router.route("/").get(getHospitals).post(createHospital);
+router
+  .route("/:id")
+  .get(getHospital)
+  .put(updateHospital)
+  .delete(deleteHospital);
 
 module.exports = router;
