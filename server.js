38d2c4c4f9a7ae973a3c9ba5197a9d37d1236342
Thanaphum Thepwan <thanaphum.tw@gmail.com>
2023-02-16 20:51:09 +0700

    style: reformat code using prettier

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const hospitals = require('./routes/hospitals');
-dotenv.config({path:'./config/config.env'});
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const hospitals = require("./routes/hospitals");
+dotenv.config({ path: "./config/config.env" });
 
 connectDB();
 
 const app = express();
 
 app.use(express.json());
-app.use('/api/v1/hospitals', hospitals);
+app.use("/api/v1/hospitals", hospitals);
 
-const PORT=process.env.PORT || 5001;
-const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, 'mode on port', PORT));
+const PORT = process.env.PORT || 5001;
+const server = app.listen(
+  PORT,
+  console.log(
+    "Server running in",
+    process.env.NODE_ENV,
+    "on http://localhost:" + PORT
+  )
+);
 
 // Handle unhandled promise rejections
-process.on('unhandledRejection',(err,promise) => {
-    console.log(`Error: ${err.message}`);
-    server.close(()=>process.exit(1));
-})
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
