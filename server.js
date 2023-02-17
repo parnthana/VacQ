19689689933b49db791acb869d39e8eb0305a505
Thanaphum Thepwan <thanaphum.tw@gmail.com>
2023-02-17 18:54:28 +0700

    feat: authentication

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const hospitals = require("./routes/hospitals");
+const auth = require("./routes/auth");
+const cookieParser = require("cookie-parser");
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
@@ -9,7 +11,9 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/auth", auth);
 
 const PORT = process.env.PORT || 5001;
 const server = app.listen(
