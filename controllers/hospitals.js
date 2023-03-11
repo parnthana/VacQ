0074be98495d18c08acec8cd26a06278694b8baf
Thanaphum Thepwan <thanaphum.tw@gmail.com>
2023-03-11 13:45:20 +0700

    feat: query and pagination

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -5,10 +5,49 @@ const Hospital = require("../models/Hospital");
 //@access   Public
 exports.getHospitals = async (req, res, next) => {
   try {
-    const hospitals = await Hospital.find();
-    res
-      .status(200)
-      .json({ success: true, count: hospitals.length, data: hospitals });
+    const reqQuery = { ...req.query };
+    const removeFields = ["select", "sort", "page", "limit"];
+    removeFields.forEach((param) => delete reqQuery[param]);
+    let queryStr = JSON.stringify(reqQuery).replace(
+      /\b(gt|gte|lt|lte|in)\b/g,
+      (match) => `$${match}`
+    );
+    let query = Hospital.find(JSON.parse(queryStr));
+    // Select
+    if (req.query.select) {
+      const fields = req.query.select.split(",").join(" ");
+      query = query.select(fields);
+    }
+    // Sort
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+    // Pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 25;
+    const startIdx = (page - 1) * limit;
+    const endIdx = page * limit;
+    const total = await Hospital.countDocuments();
+    query = query.skip(startIdx).limit(limit);
+    // Executing query
+    const hospitals = await query;
+    // Pagination result
+    const pagination = {};
+    if (endIdx < total) {
+      pagination.next = { page: page + 1, limit };
+    }
+    if (startIdx > 0) {
+      pagination.prev = { page: page - 1, limit };
+    }
+    res.status(200).json({
+      success: true,
+      count: hospitals.length,
+      pagination,
+      data: hospitals,
+    });
   } catch (err) {
     res.status(400).json({ success: false });
   }
