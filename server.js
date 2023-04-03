5fb54a8bbbc4aa4c07ce6ba99e51ab0f334947cd
Thanaphum Thepwan <thanaphum.tw@gmail.com>
2023-04-03 21:20:00 +0700

    feat: API security and Swagger

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const hospitals = require("./routes/hospitals");
 const auth = require("./routes/auth");
 const appointments = require("./routes/appointments");
 const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
@@ -12,10 +19,37 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100,
+});
+app.use(limiter);
+app.use(hpp());
 app.use(cookieParser());
 app.use("/api/v1/hospitals", hospitals);
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/appointments", appointments);
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express VacQ API",
+    },
+    servers: [
+      {
+        url: "http://localhost:5001/api/v1",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 const PORT = process.env.PORT || 5001;
 const server = app.listen(
