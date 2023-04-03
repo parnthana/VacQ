4278c2ebae4aabddc330368ec087d95fe14bb062
Thanaphum Thepwan <thanaphum.tw@gmail.com>
2023-04-03 19:53:59 +0700

    feat: connect with frontend

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const cookieParser = require("cookie-parser");
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
-
+const cors = require("cors");
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/hospitals", hospitals);
